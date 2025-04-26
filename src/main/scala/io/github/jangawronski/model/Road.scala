ca766551ab67ea790d43e1c26fafbc65ab6c346b
JanGawronski <jangawronski04@gmail.com>
2025-04-26 19:42:07 +0200

    Added Road enum

---
+++ src/main/scala/io/github/jangawronski/model/Road.scala
@@ -0,0 +1,22 @@
+package io.github.jangawronski.model
+
+enum Road:
+    case North, East, South, West
+
+    def straight: Road = this match
+        case North => South
+        case South => North
+        case East  => West
+        case West  => East
+
+    def right: Road = this match
+        case North => West
+        case South => East
+        case East  => North
+        case West  => South
+
+    def left: Road = this match
+        case North => East
+        case South => West
+        case East  => South
+        case West  => North
