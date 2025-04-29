5bb9494e1a78be1931017c79ebb4086f35d24083
JanGawronski <jangawronski04@gmail.com>
2025-04-29 19:35:47 +0200

    Added movement getter to vehicle

--- src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Vehicle.scala
+++ src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Vehicle.scala
@@ -1,3 +1,4 @@
 package io.github.jangawronski.trafficlightsimulator.model
 
-case class Vehicle(id: String, startRoad: Road, endRoad: Road, arrivalTime: Int)
+case class Vehicle(id: String, startRoad: Road, endRoad: Road, arrivalTime: Int):
+    def movement: MovementType = MovementType.derive(startRoad, endRoad)
