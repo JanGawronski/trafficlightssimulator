6790f0b1a9cfc7723d3e7398217f23c8d7042c54
JanGawronski <jangawronski04@gmail.com>
2025-04-29 17:05:41 +0200

    Added keys to CommandDto

--- src/main/scala/io/github/jangawronski/trafficlightsimulator/io/CommandDto.scala
+++ src/main/scala/io/github/jangawronski/trafficlightsimulator/io/CommandDto.scala
@@ -2,21 +2,27 @@ package io.github.jangawronski.trafficlightsimulator.io
 
 import upickle.default.{ReadWriter, macroRW}
 
+import upickle.default._
+import upickle.implicits.key
+
+@key("type")
 sealed trait CommandDto
 object CommandDto {
   implicit val rw: ReadWriter[CommandDto] = macroRW
 }
 
+@key("addVehicle")
 case class AddVehicleDto(
-  `type`: String,
   vehicleId: String,
   startRoad: String,
   endRoad:   String
 ) extends CommandDto
 object AddVehicleDto { implicit val rw: ReadWriter[AddVehicleDto] = macroRW }
 
-case class StepDto(`type`: String = "step") extends CommandDto
+@key("step")  
+case class StepDto() extends CommandDto
 object StepDto { implicit val rw: ReadWriter[StepDto] = macroRW }
 
+@key("commands")
 case class CommandsDto(commands: Seq[CommandDto])
 object CommandsDto { implicit val rw: ReadWriter[CommandsDto] = macroRW }
