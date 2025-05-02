02f119bc5bd2fad8769f4131e5a6a9e4badcfd73
JanGawronski <jangawronski04@gmail.com>
2025-05-03 01:25:09 +0200

    Added comment for Lane

--- src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Lane.scala
+++ src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Lane.scala
@@ -2,6 +2,11 @@ package io.github.jangawronski.trafficlightsimulator.model
 
 import scala.collection.mutable
 
+/**
+ * Represents a lane in the intersection.
+ * Each lane is associated with a road and can have multiple movement types.
+ * The movement types indicate the allowed movements from this lane.
+ */
 case class Lane(
   id: Int,
   road: Road,
