4b574be091e65f7dcabb0d9b2278e3672b011c4a
JanGawronski <jangawronski04@gmail.com>
2025-05-03 01:25:50 +0200

    Added comments for Vehicle

--- src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Vehicle.scala
+++ src/main/scala/io/github/jangawronski/trafficlightsimulator/model/Vehicle.scala
@@ -1,4 +1,9 @@
 package io.github.jangawronski.trafficlightsimulator.model
 
+/**
+ * Represents a vehicle in the intersection.
+ * Each vehicle has an ID, a starting road, an ending road, and an arrival time.
+ * The movement type is derived from the start and end roads.
+ */
 case class Vehicle(id: String, startRoad: Road, endRoad: Road, arrivalTime: Int):
     def movement: MovementType = MovementType.derive(startRoad, endRoad)
